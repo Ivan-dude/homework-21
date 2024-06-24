18de650bab94f0fe8f19edd7643f8d426986f8c5
Ivan-dude <ivandude87@mail.ru>
2024-06-24 22:47:05 +1000

    Add files via upload

---
+++ module_5_3(update 24.06.2024).py
@@ -0,0 +1,15 @@
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors #атрибут определяющий этажность здания
+        self.buildingType = buildingType #атрибут определяющий тип здания
+    def __eq__(self, other): #оператор прегрузки проверяющий одинаковость 2 - х атрибутов объектов между собой
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType #строгое условие
+num_floor = Building(100, 'skyscraper')
+type_building = Building(100, 'skyscraper')
+print(num_floor == type_building, ', т.к. объекты абсолютно идентичны по значениям атрибутов')
+
+num_floor = Building(100, 'skyscraper')
+type_building = Building(88, 'skyscraper')
+print(num_floor == type_building, ', т.к. значения атрибута numberOfFloors отличаются')
+
+
